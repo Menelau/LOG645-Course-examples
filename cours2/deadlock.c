a45c2f8fca1d28a250de33290dd79e21c4d3211d
Rafael Menelau <rafaelmenelau@gmail.com>
2021-09-07 01:54:41 -0400

    update deadlock example

--- cours2/deadlock.c
+++ cours2/deadlock.c
@@ -1,24 +1,24 @@
 /* simple deadlock */
 
-#include <stdio.h>
 #include <mpi.h>
+#include <stdio.h>
 
-int main(int argc, char **argv){
+int main(int argc, char* argv[]){
     int my_rank;
     MPI_Status status;
     double a[1000], b[1000];
 
-    MPI_init(&argc, &argv);
-    MPI_COMM_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     if(my_rank == 0){
         MPI_Recv(b, 1000, MPI_DOUBLE, 1, 19, MPI_COMM_WORLD, &status);
         MPI_Send(a, 1000, MPI_DOUBLE, 1, 17, MPI_COMM_WORLD);
-    } else if (my_rank == 1){
-        MPI_Recv(b, 1000, MPI_DOUBLE, 0, 17, MPI_COMM_WORLD, &status);
-        MPI_Send(a, 1000, MPI_DOUBLE, 0, 19, MPI_COMM_WORLD);
-    }
 
-    //terminate
+    } else if (my_rank == 1){
+        MPI_Recv(b, 1000, MPI_DOUBLE, 0, 19, MPI_COMM_WORLD, &status);
+        MPI_Send(a, 1000, MPI_DOUBLE, 0, 17, MPI_COMM_WORLD);
+    }
+    printf("End process.");
     MPI_Finalize();
 }

4760de4cda6149905bea2a971a3a7784cdbe50af
Rafael Menelau <rafaelmenelau@gmail.com>
2021-09-21 01:45:55 -0400

    adding return 0 to the end of the main

--- cours2/deadlock.c
+++ cours2/deadlock.c
@@ -12,13 +12,14 @@ int main(int argc, char* argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     if(my_rank == 0){
-        MPI_Recv(b, 1000, MPI_DOUBLE, 1, 19, MPI_COMM_WORLD, &status);
         MPI_Send(a, 1000, MPI_DOUBLE, 1, 17, MPI_COMM_WORLD);
+        MPI_Recv(b, 1000, MPI_DOUBLE, 1, 19, MPI_COMM_WORLD, &status);
 
     } else if (my_rank == 1){
-        MPI_Recv(b, 1000, MPI_DOUBLE, 0, 19, MPI_COMM_WORLD, &status);
-        MPI_Send(a, 1000, MPI_DOUBLE, 0, 17, MPI_COMM_WORLD);
+        MPI_Recv(b, 1000, MPI_DOUBLE, 0, 17, MPI_COMM_WORLD, &status);
+        MPI_Send(a, 1000, MPI_DOUBLE, 0, 19, MPI_COMM_WORLD);
     }
     printf("End process.");
     MPI_Finalize();
+    return 0;
 }
